51bcd80c46bf8306c6f9cb2354ac2d6d22c5dc88
9956799@gmail.com <Qqq_12345>
2022-07-01 20:13:04 +0300

    CurrentThreadExecutor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
 
@@ -35,9 +32,9 @@ public class Main {
         System.out.println(s);
     }
 
+
     // sequential execution
     static void thenApply_test() throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor(r -> new Thread(r, "dead-pool")); //r -> new Thread(r, "dead-pool")
 
         Object object = new Object();
 
@@ -45,7 +42,7 @@ public class Main {
         System.out.println(0);
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(
                 () -> {
-                        object.wait(2000);
+                        //object.wait(2000);
                         try {
                             Thread.sleep(4000);
                         } catch (InterruptedException e) {
@@ -71,21 +68,114 @@ public class Main {
         System.out.println(s);
     }
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException
+
+
+    static ExecutorService executorService = Executors.newSingleThreadExecutor(r -> new Thread(r, "dead-pool")); //r -> new Thread(r, "dead-pool")
+    static Executor es = Runnable::run; // doesnt work
+
+
+    // !!!!!!!!!!!!!!!!!!!! Current Thread Executor !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    static class CurrentThreadExecutor implements Executor {
+        public void execute(Runnable r) {
+            r.run();
+        }
+    }
+
+
+    public static void main(String[] args)
     {
         //thenCombine_test();
-        thenApply_test();
+        //thenApply_test();
 
         int a = 1;
 
 
 
-//        Future<String> completableFuture =
-//                CompletableFuture.completedFuture("Hello");
-//
-//        String result = completableFuture.get();
-//        assertEquals("Hello", result);
+//        CompletableFuture<Void> completableFuture =
+//                CompletableFuture.completedFuture("Hello").thenAccept((v)->{
+//                    System.out.println(v);
+//                });
 
+        //String result = completableFuture.get();
+        //assertEquals("Hello", result);
+
+
+        Executor currentThreadExecutor = new CurrentThreadExecutor();
+
+        System.out.println("Begin");
+        CompletableFuture<Integer> cf2 = CompletableFuture.supplyAsync(//.completedFuture(1
+                () ->  {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("supplyAsync " + Thread.currentThread().getName());
+            return 1;
+        }, currentThreadExecutor//, executorService
+        );
+        CompletableFuture cf3 = cf2.thenApplyAsync( //thenAcceptAsync( // no res
+                    v -> {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("thenApplyAsync " + v + ", " + Thread.currentThread().getName());
+                        //int q = 1/0;
+                        return 2;
+                    }, currentThreadExecutor //, executorService
+            )
+            .exceptionally(ex -> {
+                System.out.println("exceptionally " + ex + ", " + Thread.currentThread().getName());
+                return 3;
+           })
+//                .thenAcceptBoth(CompletableFuture.completedFuture(1) //.supplyAsync(() -> " World"),
+//                        , (s1, s2) -> {
+//                            int qq = 1;
+//                            //return null;
+//                        }
+//                )
+                .thenCombine(CompletableFuture.supplyAsync(()-> {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return 10;
+                    }
+                    , currentThreadExecutor
+                    ) //.supplyAsync(() -> " World"),
+                    , (s1, s2) -> {
+                    return s1 + s2;
+                    }
+                )
+                .thenCompose(q -> CompletableFuture.completedFuture(q+1))
+                ;
+
+        //cf3.handle()
+//        try {
+//            cf3.join();
+//        } catch (CompletionException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            cf3.get();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+
+//        System.out.println("Joined ");
+
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("End " + cf3.isDone() + " " + cf3.isCompletedExceptionally());
 
 
 //        CompletableFuture<String> future1
